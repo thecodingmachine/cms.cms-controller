21db56ffcc909e4eab86ed92e2bbc3c3a70fa7ac
Kevin Nguyen <k.nguyen@thecodingmachine.com>
2013-07-02 10:48:36 +0200

    Adding Flash + custom upload video plugin for full config
    Signed-off-by: Kevin Nguyen <k.nguyen@thecodingmachine.com>

--- src/js/ck_full_config.js
+++ src/js/ck_full_config.js
@@ -47,7 +47,7 @@ CKEDITOR.editorConfig = function( config ) {
 	          { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
 	          { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl' ] },
 	          { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
-	          { name: 'insert', items: [ 'Image', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
+	          { name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe', 'Jwp' ] },
 	          '/',
 	          { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
 	          { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
@@ -69,5 +69,8 @@ CKEDITOR.editorConfig = function( config ) {
                { name: 'colors' },
                { name: 'tools' },
                { name: 'others' }
-	]
+	];
+	
+	config.extraPlugins = 'jwp';
 }
+
